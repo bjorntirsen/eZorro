a098ab80c46dc4d71a4c1ded2799ad64cdeb4e25
Björn Tirsén <bjorntirsen@gmail.com>
2020-12-10 15:34:38 +0100

    Add headlines to Currencies Page

--- src/pages/CurrenciesPage.jsx
+++ src/pages/CurrenciesPage.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function CurrenciesPage() {
     return (
         <div>
-            currencies
+            <h2>Currencies</h2>
+            <h3>This is the Currencies Page</h3>
         </div>
     )
 }

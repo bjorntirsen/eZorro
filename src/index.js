c97bc81475e50957f9b28bd80b4594e6447e8f95
Björn Tirsén <bjorntirsen@gmail.com>
2020-12-14 14:37:31 +0100

    ✨Add custom colour palette to bootstrap

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import 'bootstrap/dist/css/bootstrap.css'
+import './styles/CurrencyStyles.scss'
 
 ReactDOM.render(
   <React.StrictMode>

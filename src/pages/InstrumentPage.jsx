2e36b476a3fea0169fb654c131a5a8b71532dfca
Dino Pacariz <dinopacariz@gmail.com>
2020-12-18 06:45:26 +0100

    first commit

--- src/pages/InstrumentPage.jsx
+++ src/pages/InstrumentPage.jsx
@@ -1,4 +1,5 @@
 import React , { useEffect , useState } from 'react'
+import ArrowIconStyled from '../components/ArrowIconStyled'
 
 export default function InstrumentPage(props) {
     const [stock , setStock] = useState(null)
@@ -19,14 +20,41 @@ export default function InstrumentPage(props) {
             
             {stock && 
                 <>
-                    <h2>{stock.name}</h2>
-                    <p>{stock.source}</p>
+                    <div>
+                        <h2>{stock.name}</h2>
+                        <p>{stock.source}</p>
+                    </div>
                     <div>
                         <span>Price: $</span>
                         <span>{stock.price}</span>
                     </div>
                     <div>
-                        <span>{}</span>
+                        <span>Today: </span>
+                        <span>{stock.today}</span>
+                        <ArrowIconStyled value={stock.today} />
+                    </div>
+                    <div>
+                        <h1>History</h1>
+                        <div>
+                            <span>1 week: </span>
+                            <span>{stock.w1}</span>
+                            <ArrowIconStyled value={stock.w1} />
+                        </div>
+                        <div>
+                            <span>1 month: </span>
+                            <span>{stock.mtd}</span>
+                            <ArrowIconStyled value={stock.mtd} />
+                        </div>
+                        <div>
+                            <span>3 years: </span>
+                            <span>{stock.y3}</span>
+                            <ArrowIconStyled value={stock.y3} />
+                        </div>
+                        <div>
+                            <span>5 years: </span>
+                            <span>{stock.y5}</span>
+                            <ArrowIconStyled value={stock.y5} />
+                        </div>
                     </div>
                 </>
             }

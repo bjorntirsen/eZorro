f055b96de67ce6430ab7424881f49e500ac1d86f
Björn Tirsén <bjorntirsen@gmail.com>
2020-12-18 15:51:51 +0100

    🐛Fixed bug with ArrowIconStyled.jsx

--- src/components/ArrowIconStyled.jsx
+++ src/components/ArrowIconStyled.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import styled from "styled-components"
 
-export default function ArrowIconStyled({value}) {
-    
+const ArrowIconStyled = styled.i`
+    color: ${props => props.arrowColor}
+`
+export default function ArrowIcon({value}) {
     function setArrowType(number) {
         if (number === 0) return "fas fa-arrow-circle-right fa-2x m-1"
         else if (number < 0) return "fas fa-arrow-circle-down fa-2x m-1"
         else return "fas fa-arrow-circle-up fa-2x m-1"
     }
-
     function setArrowColor(number) {
         if (number === 0) return "orange"
         else if (number < 0) return "red"
         else return "green"
     }
-
     const arrowColor = setArrowColor(value)
-
-    const ArrowIconStyled = styled.i`
-        color: ${arrowColor}
-    `
-
     return (
         <ArrowIconStyled
+            arrowColor={arrowColor}
             className={setArrowType(value)}
         />
     )

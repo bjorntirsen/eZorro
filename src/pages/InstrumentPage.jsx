b27ee9def3e7cb61a3d71b8b6b632debd2bec019
Dino Pacariz <dinopacariz@gmail.com>
2020-12-18 10:07:53 +0100

    working on alignment

--- src/pages/InstrumentPage.jsx
+++ src/pages/InstrumentPage.jsx
@@ -12,48 +12,88 @@ export default function InstrumentPage(props) {
         .then(data => setStock(data))
     }, [path])
     
+    function nickName(ticker) {
+        const split = ticker.split("-")
+        return split[0]
+    }
+
     return (
 
         <div>
             {!stock && <p>Loading</p>}
             {stock && console.log(stock)}
-            
             {stock && 
                 <>
-                    <div>
-                        <h2>{stock.name}</h2>
-                        <p>{stock.source}</p>
-                    </div>
-                    <div>
-                        <span>Price: $</span>
-                        <span>{stock.price}</span>
-                    </div>
-                    <div>
-                        <span>Today: </span>
-                        <span>{stock.today}</span>
-                        <ArrowIconStyled value={stock.today} />
-                    </div>
-                    <div>
-                        <h1>History</h1>
-                        <div>
-                            <span>1 week: </span>
-                            <span>{stock.w1}</span>
-                            <ArrowIconStyled value={stock.w1} />
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 d-flex flex-row align-items-center
+                                            justify-content-center">
+                                <h3>{nickName(stock.ticker)}</h3>
+                                <h4 className="ml-2 mr-2">|</h4>
+                                <h4>{stock.name}</h4>
+                            </div>
                         </div>
-                        <div>
-                            <span>1 month: </span>
-                            <span>{stock.mtd}</span>
-                            <ArrowIconStyled value={stock.mtd} />
-                        </div>
-                        <div>
-                            <span>3 years: </span>
-                            <span>{stock.y3}</span>
-                            <ArrowIconStyled value={stock.y3} />
-                        </div>
-                        <div>
-                            <span>5 years: </span>
-                            <span>{stock.y5}</span>
-                            <ArrowIconStyled value={stock.y5} />
+                        <div className="row">
+                            <div className="col-sm-12 col-md-6">
+                                <div className="row">
+                                    <h4 className="mr-2">Price:</h4>
+                                    <h4>{stock.price}</h4>
+                                </div>
+                                <div className="row">
+                                    <h4 className="mr-2">Today:</h4>
+                                    <h4>{stock.today}</h4>
+                                    <ArrowIconStyled value={stock.today} />
+                                </div>
+                            </div>
+                            <div className="col-sm-12 col-md-6">
+                                <h4>History</h4>
+                                <div className="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Since</th>
+                                            <th scope="col">+/-%</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1 week</td>
+                                            <td>{stock.w1}</td>
+                                            <td className="py-1">
+                                                <ArrowIconStyled value={stock.w1} />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 month</td>
+                                            <td>{stock.mtd}</td>
+                                            <td className="py-1">
+                                                <ArrowIconStyled value={stock.mtd} />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 year</td>
+                                            <td>{stock.ytd}</td>
+                                            <td className="py-1">
+                                                <ArrowIconStyled value={stock.ytd} />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>3 years</td>
+                                            <td>{stock.y3}</td>
+                                            <td className="py-1">
+                                                <ArrowIconStyled value={stock.y3} />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>5 years</td>
+                                            <td>{stock.y5}</td>
+                                            <td className="py-1">
+                                                <ArrowIconStyled value={stock.y5} />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </>

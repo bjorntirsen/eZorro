398ea9601e1b259bbd10da9030d7e0aa94ebd042
Björn Tirsén <bjorntirsen@gmail.com>
2020-12-20 19:52:26 +0100

    ♻️Refactored ArrowIconStyled.jsx

--- src/components/ArrowIconStyled.jsx
+++ src/components/ArrowIconStyled.jsx
@@ -4,21 +4,23 @@ import styled from "styled-components"
 const ArrowIconStyled = styled.i`
     color: ${props => props.arrowColor}
 `
+
+function setArrowColor(number) {
+    if (number === 0) return "orange"
+    else if (number < 0) return "red"
+    else return "green"
+}
+
+function setArrowType(number) {
+    if (number === 0) return "fas fa-arrow-circle-right fa-2x m-1"
+    else if (number < 0) return "fas fa-arrow-circle-down fa-2x m-1"
+    else return "fas fa-arrow-circle-up fa-2x m-1"
+}
+
 export default function ArrowIcon({value}) {
-    function setArrowType(number) {
-        if (number === 0) return "fas fa-arrow-circle-right fa-2x m-1"
-        else if (number < 0) return "fas fa-arrow-circle-down fa-2x m-1"
-        else return "fas fa-arrow-circle-up fa-2x m-1"
-    }
-    function setArrowColor(number) {
-        if (number === 0) return "orange"
-        else if (number < 0) return "red"
-        else return "green"
-    }
-    const arrowColor = setArrowColor(value)
     return (
         <ArrowIconStyled
-            arrowColor={arrowColor}
+            arrowColor={setArrowColor(value)}
             className={setArrowType(value)}
         />
     )
